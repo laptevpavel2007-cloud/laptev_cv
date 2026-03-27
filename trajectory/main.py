c0faeb170281339997ab7e036afdf4e6a21118cb
Neko_LPN <laptevpavel2007@gmail.com>
2026-03-27 22:29:24 +0800

    update trajectory

--- trajectory/main.py
+++ trajectory/main.py
@@ -6,16 +6,8 @@ def centroid(labeled, label = 1):
     y, x = np.where(labeled == label)
     return np.mean(y), np.mean(x)
 
-x_all = []
-y_all = []
-for i in range(100):
-    image = np.load(f"./motion/out/h_{i}.npy")
-    labeled = label(image)
-    for j in range(1, np.max(labeled)+1):
-        l = labeled == j
-        y, x = centroid(l)
-        x_all.append(x)
-        y_all.append(y)
+def distance(x1, x2, y1, y2):
+    return ((x1 - x2)**2 + (y1 - y2)**2) ** 0.5
 
 x_1 = []
 y_1 = []
@@ -23,20 +15,55 @@ x_2 = []
 y_2 = []
 x_3 = []
 y_3 = []
-flag = 1
-for i in range(len(x_all)):
-    if flag == 1:
-        x_1.append(x_all[i])
-        y_1.append(y_all[i])
-        flag += 1
-    elif flag == 2:
-        x_2.append(x_all[i])
-        y_2.append(y_all[i])
-        flag += 1 
-    elif flag == 3:
-        x_3.append(x_all[i])
-        y_3.append(y_all[i])
-        flag = 1
+
+image = np.load("./motion/out/h_0.npy")
+labeled = label(image)
+for j in range(1, np.max(labeled)+1):
+    y, x = centroid(labeled, j)
+    if y is not None and x is not None:
+        if j == 1:
+            y_1.append(y)
+            x_1.append(x)
+        elif j == 2:
+            y_2.append(y)
+            x_2.append(x)
+        elif j == 3:
+            y_3.append(y)
+            x_3.append(x)
+
+
+for i in range(1, 100):
+    image = np.load(f"./motion/out/h_{i}.npy")
+    labeled = label(image)
+
+    for j in range(1, np.max(labeled)+1):
+        y, x = centroid(labeled, j)
+
+        dist = float('inf')
+        dist1 = distance(x, x_1[-1], y, y_1[-1])
+        if dist1 < dist and dist1 <= 20:
+            dist = dist1
+            index = 1
+                    
+        dist2 = distance(x, x_2[-1], y, y_2[-1])
+        if dist2 < dist and dist2 <= 20:
+            dist = dist2
+            index = 2
+
+        dist3 = distance(x, x_3[-1], y, y_3[-1])
+        if dist3 < dist and dist3 <= 20:
+            dist = dist3
+            index = 3
+                  
+        if index == 1:
+            y_1.append(y)
+            x_1.append(x)
+        elif index == 2:
+            y_2.append(y)
+            x_2.append(x)
+        elif index == 3:
+            y_3.append(y)
+            x_3.append(x)
 
 plt.plot(x_1, y_1, 'y-')
 plt.plot(x_2, y_2, 'g-')
